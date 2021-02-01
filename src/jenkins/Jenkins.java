1fe9ee711c08c2effb467f48f5333d12adffa749
Jordi Diaz <jodini@inf1-jodini.fjeclot.net>
2021-02-01 20:31:35 +0100

    Primera modificació de la prova d'IC amb Jenkins

--- src/jenkins/Jenkins.java
+++ src/jenkins/Jenkins.java
@@ -2,10 +2,11 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package jenkins;
+package jenkins
 /**
  *
- * @author jodinidaw2021@
+ * @author jodini2021daw2@gmail.com
+
  */
 import java.io.*;
 public class Jenkins {
